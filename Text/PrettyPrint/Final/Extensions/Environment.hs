c2c8a4c72d4a79ffe37426d37c524b27d9772a5a
David Raymond Christiansen <david@davidchristiansen.dk>
2017-05-23 15:45:27 -0400

    Extension Haddock

--- Text/PrettyPrint/Final/Extensions/Environment.hs
+++ Text/PrettyPrint/Final/Extensions/Environment.hs
@@ -11,7 +11,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module Text.PrettyPrint.Final.Extensions.Environment where
+{-|
+Module      : Text.PrettyPrint.Final.Extensions.Environment
+Description : Lexical scope tracking for final pretty printers
+Copyright   : (c) David Darais, David Christiansen, and Weixi Ma 2016-2017
+License     : MIT
+Maintainer  : david.darais@gmail.com
+Stability   : experimental
+Portability : Portable
+
+'EnvT' extends a pretty printer to track a lexical environment.
+-}
+
+module Text.PrettyPrint.Final.Extensions.Environment
+  ( MonadPrettyEnv(..)
+  , MonadReaderEnv(..)
+  -- * The transformer
+  , EnvT(..)
+  , runEnvT
+  , mapEnvT
+  ) where
 
 import Control.Applicative
 import Control.Monad.Reader
@@ -22,25 +41,34 @@ import Control.Monad.Writer
 import Text.PrettyPrint.Final as Final
 import Text.PrettyPrint.Final.Extensions.Precedence
 
+-- | A reader of environments
 class MonadReaderEnv env m | m -> env where
+  -- | See 'ask'
   askEnv :: m env
+  -- | See 'local'
   localEnv :: (env -> env) -> m a -> m a
 
+-- | Pretty monads that can read environments. Use this to implement
+-- lexical scope in your pretty printer, because the dynamic extent of
+-- pretty monad computations typically corresponds to the scope of a
+-- binder.
 class ( MonadPretty w ann fmt m
       , MonadReaderEnv env m
       ) => MonadPrettyEnv env w ann fmt m
       | m -> w, m -> ann, m -> fmt, m -> env where
 
-
+-- | A transformer that adds a reader effect, distinguished by the newtype tag.
 newtype EnvT env m a = EnvT { unEnvT :: ReaderT env m a }
   deriving
     ( Functor, Monad, Applicative, Alternative, MonadTrans
     , MonadState s, MonadWriter o
     )
 
+-- | Run a pretty printer in an initial environment
 runEnvT :: env -> EnvT env m a -> m a
 runEnvT e xM = runReaderT (unEnvT xM) e
 
+-- | Transform the result of a pretty printer
 mapEnvT :: (m a -> n b) -> EnvT env m a -> EnvT env n b
 mapEnvT f = EnvT . mapReaderT f . unEnvT
 
--- Text/PrettyPrint/Final/Extensions/Precedence.hs
+++ Text/PrettyPrint/Final/Extensions/Precedence.hs
@@ -11,14 +11,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE UndecidableInstances #-}
 
+{-|
+Module      : Text.PrettyPrint.Final.Extensions.Precedence
+Description : A pretty printer extension for tracking precedence and associativity
+Copyright   : (c) David Darais, David Christiansen, and Weixi Ma 2016-2017
+License     : MIT
+Maintainer  : david.darais@gmail.com
+Stability   : experimental
+Portability : Portable
+
+A transformer of pretty monads that provides effects for inserting
+parentheses minimally and correctly.
+-}
 module Text.PrettyPrint.Final.Extensions.Precedence
-  ( PrecEnv(..)
+  ( -- * Precedence information
+    PrecEnv(..)
   , precEnv0
-  , MonadReaderPrec(..)
-  , MonadPrettyPrec(..)
-  , PrecT(..)
-  , runPrecT
-  , mapPrecT
+    -- * Precedence effects
   , askLevel
   , localLevel
   , infl
@@ -27,6 +36,12 @@ module Text.PrettyPrint.Final.Extensions.Precedence
   , botLevel
   , app
   , askBumped
+  -- * The transformer
+  , MonadReaderPrec(..)
+  , MonadPrettyPrec(..)
+  , PrecT(..)
+  , runPrecT
+  , mapPrecT
   ) where
 
 import Control.Monad
@@ -42,15 +57,23 @@ import qualified Data.Text as T
 
 import Text.PrettyPrint.Final as Final
 
--- Environment for Prec extension
-
+-- | A precedence environment contains enough information to determine
+-- whether parentheses should be inserted.
 data PrecEnv ann = PrecEnv
   { level :: Int
+    -- ^ The current precedence level of the context
   , bumped :: Bool
+    -- ^ A tiebreaker used to distinguish left- and right-associative contexts
   , lparen :: (Text, Maybe ann)
+    -- ^ What to show for a left parenthesis. The optional annotation
+    -- will be applied.
   , rparen :: (Text, Maybe ann)
+    -- ^ What to show for a right parenthesis. The optional annotation
+    -- will be applied.
   }
 
+-- | An initial precedence environment that works for languages with
+-- parentheses as delimiters.
 precEnv0 :: PrecEnv ann
 precEnv0 = PrecEnv
   { level = 0
@@ -59,18 +82,24 @@ precEnv0 = PrecEnv
   , rparen = (")", Nothing)
   }
 
--- Type class for Prec extension
 
+-- | Precedence follows the structure of a document, so a Reader
+-- provides the appropriate dynamic extent of precedence information.
 class MonadReaderPrec ann m | m -> ann where
+  -- | What is the current precedence environment? (see 'ask')
   askPrecEnv :: m (PrecEnv ann)
+  -- | Override the precedence environment in a subcomputation. (see 'local')
   localPrecEnv :: (PrecEnv ann -> PrecEnv ann) -> m a -> m a
 
+-- | What is the current precedence level?
 askLevel :: (Functor m, MonadReaderPrec ann m) => m Int
 askLevel = level <$> askPrecEnv
 
+-- | Run a subcomputation with a modified precedence level.
 localLevel :: (Functor m, MonadReaderPrec ann m) => (Int -> Int) -> m a -> m a
 localLevel f = localPrecEnv $ \ pe -> pe { level = f $ level pe }
 
+-- | Is the current precedence bumped? See 'PrecEnv'.
 askBumped :: (Functor m, MonadReaderPrec ann m) => m Bool
 askBumped = bumped <$> askPrecEnv
 
@@ -89,27 +118,36 @@ askRParen = rparen <$> askPrecEnv
 localRParen :: (Functor m, MonadReaderPrec ann m) => ((Text, Maybe ann) -> (Text, Maybe ann)) -> m a -> m a
 localRParen f = localPrecEnv $ \ pe -> pe { rparen = f $ rparen pe }
 
-class (MonadPretty w ann fmt m, MonadReaderPrec ann m) => MonadPrettyPrec w ann fmt m | m -> w, m -> ann, m -> fmt
+-- | A pretty monad that can read precedence environments
+class ( MonadPretty w ann fmt m
+      , MonadReaderPrec ann m
+      ) =>
+      MonadPrettyPrec w ann fmt m
+      | m -> w, m -> ann, m -> fmt
 
 -- Operations
 
+-- | Put a subdocument in the lowest precedence context
 botLevel :: (MonadPrettyPrec w ann fmt m) => m () -> m ()
 botLevel = localLevel (const 0) . localBumped (const False)
 
+-- | Close a context with left and right delimiters
 closed :: (MonadPrettyPrec w ann fmt m) => m () -> m () -> m () -> m ()
 closed alM arM aM = do
   alM
   botLevel $ aM
   arM
 
+-- | Close a context with the configured left and right parentheses
 parens :: (MonadPrettyPrec w ann fmt m) => m () -> m ()
 parens aM = do
   (lp, lpA) <- askLParen
-  (rp, rpA) <- askRParen 
-  let lpD = maybe id annotate lpA $ text lp 
+  (rp, rpA) <- askRParen
+  let lpD = maybe id annotate lpA $ text lp
       rpD = maybe id annotate rpA $ text rp
   closed lpD rpD $ align aM
 
+-- | Run a subcomputation at a particular precedence level
 atLevel :: (MonadPrettyPrec w ann fmt m) => Int -> m () -> m ()
 atLevel i' aM = do
   i <- askLevel
@@ -119,41 +157,51 @@ atLevel i' aM = do
     then aM'
     else parens aM'
 
+-- | Bump the precedence to implement associativity
 bump :: (MonadPrettyPrec w ann fmt m) => m a -> m a
 bump = localBumped $ const True
 
+-- | Display a non-associative infix operator at a precedence level
 inf :: (MonadPrettyPrec w ann fmt m) => Int -> m () -> m () -> m () -> m ()
 inf i oM x1M x2M = atLevel i $ bump x1M >> space 1 >> oM >> space 1 >> bump x2M
 
+-- | Display a left-associative infix operator at a precedence level
 infl :: (MonadPrettyPrec w ann fmt m) => Int -> m () -> m () -> m () -> m ()
 infl i oM x1M x2M = atLevel i $ x1M >> space 1 >> oM >> space 1 >> bump x2M
 
+-- | Display a right-associative infix operator at a precedence level
 infr :: (MonadPrettyPrec w ann fmt m) => Int -> m () -> m () -> m () -> m ()
 infr i oM x1M x2M = atLevel i $ bump x1M >> space 1 >> oM >> space 1 >> x2M
 
+-- | Display a prefix operator at a precedence level
 pre :: (MonadPrettyPrec w ann fmt m) => Int -> m () -> m () -> m ()
 pre i oM xM = atLevel i $ oM >> space 1 >> xM
 
+-- | Display a postfix operator at a precedence level
 post :: (MonadPrettyPrec w ann fmt m) => Int -> m () -> m () -> m ()
 post i oM xM = atLevel i $ xM >> space 1 >> oM
 
+-- | Perform function application with precedence level <100
 app :: (MonadPrettyPrec w ann fmt m) => m () -> [m ()] -> m ()
 app x xs = atLevel 100 $ hvsep $ x : map (align . bump) xs
 
+-- | Lay out a collection like 'Final.collection', but reset the precedence level.
 collection :: (MonadPrettyPrec w ann fmt m) => m () -> m () -> m () -> [m ()] -> m ()
 collection open close sep = Final.collection open close sep . map botLevel
 
 -- Monad Transformer
-
+-- | A monad transformer that adds a precedence effects
 newtype PrecT ann m a = PrecT { unPrecT :: ReaderT (PrecEnv ann) m a }
   deriving
   ( Functor, Monad, Applicative, Alternative, MonadTrans
   , MonadState s, MonadWriter o
   )
 
+-- | Run a precedence transformer with some initial precedence environment
 runPrecT :: PrecEnv ann -> PrecT ann m a -> m a
 runPrecT pr xM = runReaderT (unPrecT xM) pr
 
+-- | Transform the value returned by a 'PrecT'
 mapPrecT :: (m a -> n b) -> PrecT ann m a -> PrecT ann n b
 mapPrecT f = PrecT . mapReaderT f . unPrecT
 
